f529454081b701b643dc5ce5249476eefe67d7d1
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2024-02-08 21:15:45 -0300

    mais cantores add ao sistema de busca

--- scripts.js
+++ scripts.js
@@ -1,34 +1,34 @@
 const searchInput = document.querySelector('#search-input');
-const resultArtists = document.querySelector('#result-artists')
-const resultPlaylist = document.querySelector('#results-playlist')
+const resultArtists = document.querySelector('#result-artists');
+const resultPlaylist = document.querySelector('#results-playlist');
 
-function request (searchTerm) {
-    const url = `http://localhost/artists.json?name_like=${searchTerm}`;
+function request(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
-    .then(response => response.json())
-    .then(json => displayResults(json))
+        .then((response) => response.json())
+        .then((json) => displayResults(json));
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden");
+    resultPlaylist.classList.add('hidden');
     const artistsName = document.querySelector('#artist-name');
     const artistsImg = document.querySelector('#artist-img');
 
-    result.forEach(e => {
+    result.forEach((e) => {
         artistsName.innerText = e.name;
         artistsImg.src = e.urlImg;
     });
 
-    resultArtists.classList.remove('hidden')
+    resultArtists.classList.remove('hidden');
 }
 
-document.addEventListener('input', e => {
-    const value = searchInput.value.toLowerCase()
-    if(value === '') {
+document.addEventListener('input', (e) => {
+    const value = searchInput.value.toLowerCase();
+    if (value === '') {
         resultPlaylist.classList.remove('hidden');
         resultArtists.classList.add('hidden');
-        return
+        return;
     }
 
     request(value);
-})
+});
